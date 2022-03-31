c4eaa35c0b39639d7975c9d1bbe52b4a02d7bd22
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-03-31 20:15:50 +0800

    添加数据存储类

---
+++ GreatQr/DataSaver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace GreatQr
+{
+    public class DataSaver
+    {
+        public string key;
+        public DataSaver()
+        {
+        }
+        public void setKey(string dataKey)
+        {
+            key = dataKey;
+        }
+        public string getKey()
+        {
+            return key;
+        }
+        public async Task<bool> setStringValue(string value)
+        {
+            try
+            {
+                await SecureStorage.SetAsync(key, value);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+        public async Task<string> getStringValue()
+        {
+            try
+            {
+                return await SecureStorage.GetAsync(key);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+        public bool removeValue()
+        {
+            try
+            {
+                SecureStorage.Remove(key);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}

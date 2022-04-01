152e1b782d2cf4a70754c3acae380df476952120
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-04-01 10:24:45 +0800

    添加设置界面

---
+++ GreatQr/SettingsActivity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using Debug = System.Diagnostics.Debug;
+
+namespace GreatQr
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class SettingsActivity: AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_settings);
+        }
+    }
+}

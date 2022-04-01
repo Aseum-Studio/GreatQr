76f14892230377f1f5f60cf6264e5575b798b747
bcm-ray_crazy <65066644+bcmRayCrazy-coder@users.noreply.github.com>
2022-04-01 10:52:56 +0800

    添加更换图片基本框架

---
+++ GreatQr/ChangePicController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+namespace GreatQr
+{
+    public class ChangePicController
+    {
+        private AppCompatActivity mActivity;
+        public ChangePicController(AppCompatActivity activity)
+        {
+            mActivity = activity;
+        }
+        public void changePic()
+        {
+            mActivity.Finish();
+        }
+    }
+}
